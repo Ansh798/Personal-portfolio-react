01170eac9ed4804838a59ef75484e4732fe37bb9
Ansh798 <amshumankumar798@gmail.com>
2023-06-25 18:27:53 +0530

    customized

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,6 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -50,12 +48,10 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/anshuman-kumar-8a82a6237/"><img src={navIcon1} alt="" /></a>
               </div>
               <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+                <button className="vvd"><span>Let's Connect</span></button>
               </HashLink>
             </span>
           </Navbar.Collapse>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -34,8 +34,23 @@ export const Skills = () => {
             <div className="row">
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <h2>Skills  and Experience</h2>
+                        <p>
+                          I have 1 year of experience in React js, Python(Django), Node js. <br></br>
+                          <strong>In react</strong>, i have a deep understanding of React's component-based architecture, enabling me to create reusable and modular UI components.
+                            I am experienced in managing application state using libraries such as Redux.
+                            I have utilized React Router to implement dynamic routing within applications.
+                            I have integrated React applications with RESTful APIs and have experience in handling asynchronous data fetching using libraries like Axios or the built-in Fetch API
+                          <br></br>
+                          <strong>In Python</strong>, i possess a solid foundation in Python and have used Django extensively to build server-side logic for web applications. 
+                            I am well-versed in Django's ORM (Object-Relational Mapping), allowing me to design and interact with databases efficiently.  I have designed and implemented RESTful APIs using Django REST Framework. 
+                            I understand the importance of well-designed APIs for seamless communication between the front-end and back-end, enabling smooth integration with various clients.
+                            I have experience working with databases such as MySQL through Django's ORM. I can design database schemas, create efficient queries, and handle data migrations effectively.
+                            <br></br>
+                          <strong>In Node js</strong>, i am familiar with the event-driven, non-blocking nature of Node.js that enables high scalability and performance.
+                            I have worked with frameworks like Express.js to create server routes, handle middleware, and interact with databases.
+                            I am comfortable with asynchronous programming, using promises or async/await, and have experience with real-time applications using libraries like Socket.io.
+                          </p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
@@ -43,15 +58,15 @@ export const Skills = () => {
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>Frontend(React js)</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>Backend(Python/Django)</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Backend(Node js/Express js)</h5>
                             </div>
                         </Carousel>
                     </div>
